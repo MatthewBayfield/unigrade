26fbf15f7818521464ead091602903293bf5b918
MatthewBayfield <matthew.bayfield@yahoo.com>
2022-04-25 16:25:55 +0000

    Create display_top_level_interface function
    Displays to the user the unigrade logo, and prompts them to choose
    through input, either to view module or student information.

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from os import system
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -12,3 +13,32 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('unigrade-physics')
 
+
+def display_top_level_interface():
+    """
+    Displays the top-level interface, after clearing the console. The user is prompted to choose one of two options: either view module information, or view and or edit/add student information
+    """
+    system('clear')
+    print("""
+         _    _  _   _  _____  _____  _____             _____   ______ 
+        | |  | || \ | ||_   _|/ ____||  __ \     /\    |  __ \ |  ____|
+        | |  | ||  \| |  | | | |  __ | |__) |   /  \   | |  | || |__   
+        | |  | || . ` |  | | | | |_ ||  _  /   / /\ \  | |  | ||  __|  
+        | |__| || |\  | _| |_| |__| || | \ \  / ____ \ | |__| || |____ 
+         \____/ |_| \_||_____|\_____||_|  \_\/_/    \_\|_____/ |______|
+    \n""")
+    print("To view module information and statistics, enter 1.\n")
+    print("To view or add/edit student information enter 2.\n")
+    while True:
+        selected_option = input("->")
+        if selected_option == '1':
+            print('Loading module interface.')
+            break
+        elif selected_option == '2':
+            print('Loading student information interface.')
+            break
+        else:
+            print('Invalid input. Please enter either 1 or 2.\n')
+
+
+display_top_level_interface()
