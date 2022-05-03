1c47420a36d71bfe876a4b42bf3d55bbf6287352
MatthewBayfield <matthew.bayfield@yahoo.com>
2022-05-03 20:23:51 +0000

    Add student_registration_interface func to main func global dictionaries

--- run.py
+++ run.py
@@ -271,16 +271,16 @@ def main():
     """
     global FUNCTION_DICTIONARY
     FUNCTION_DICTIONARY = {'top_level_interface': top_level_interface, 'student_information_top_level_interface': student_information_top_level_interface,
-                           'modules_interface': modules_interface,  'register_student': register_student, 'unregister_student': unregister_student,
+                           'modules_interface': modules_interface, 'student_registration_interface': student_registration_interface,
+                            'register_student': register_student, 'unregister_student': unregister_student,
                             'exit_the_program': sys.exit, 'go_back' : go_back}
     
     global FUNCTION_USER_DESCRIPTION_DICTIONARY
     FUNCTION_USER_DESCRIPTION_DICTIONARY = {'top_level_interface': 'return to the top program interface',
                                         'student_information_top_level_interface': 'view or add/edit student information',
-                                        'modules_interface': 'view module information and statistics', 'register_student': 'register the student',
+                                        'modules_interface': 'view module information and statistics','student_registration_interface': 'register or unregister a student',                                        'register_student': 'register the student',
                                         'unregister_student': 'unregister the student', 'exit_the_program': 'exit the unigrade program',
                                         'go_back': 'go back'}
-    
     global last_function_call
     last_function_call = ''
     global next_function_call
