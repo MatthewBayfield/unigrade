44aa33a3bd0f3b04f529959ba63ac0686c14b2b8
MatthewBayfield <matthew.bayfield@yahoo.com>
2022-05-15 20:32:33 +0000

    Add try-except statements for opening google sheet in academic_module

--- modules/classes/academic_module.py
+++ modules/classes/academic_module.py
@@ -1,6 +1,12 @@
+import sys
+import os
 import gspread
 from google.oauth2.service_account import Credentials
-
+academic_module_dir = os.path.dirname(__file__)
+general_functions_dir = os.path.join(academic_module_dir, '..')
+sys.path.insert(1, general_functions_dir)
+import general_functions as gen_functions
+import decorated_gspread_methods
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -8,10 +14,23 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('unigrade-physics')
+try:
+    CREDS = Credentials.from_service_account_file('creds.json')
+    SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+    GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+    SHEET = GSPREAD_CLIENT.open('unigrade-physics')
+except gspread.exceptions.GSpreadException:
+    print('''ERROR ENCOUNTERED: There seems to be a problem accessing
+the unigrade google sheet. The unigrade program will now terminate.
+Please try running the program again. If the error persists try again later.\n''')
+    print('Enter any key to initiate exiting the unigrade program.')
+    input('->')
+    system('clear')
+    print('Quitting the unigrade program...')
+    time.sleep(3.0)
+    system('clear')
+    sys.exit()
+
 
 
 
@@ -26,5 +45,5 @@ class AcademicModule:
         Produces a list of academic module titles for a chosen year x: 1<=x<=4, retrieved from the unigrade google sheet.
         """
         YEAR_X_MODULES_WORKSHEET = SHEET.worksheet(f'year {x} modules')
-        year_x_modules = list(filter(lambda title: title != "", YEAR_x_MODULES_WORKSHEET.row_values(1)))
+        year_x_modules = list(filter(lambda title: title != "", YEAR_X_MODULES_WORKSHEET.row_values(1)))
         return year_x_modules
