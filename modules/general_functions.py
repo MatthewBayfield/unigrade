f201ed463dd713968042e9e87bb385c0b770192f
MatthewBayfield <matthew.bayfield@yahoo.com>
2022-05-15 17:20:14 +0000

    Remove the relocated gspread_api_error_exception_handling decorator func

--- modules/general_functions.py
+++ modules/general_functions.py
@@ -1,7 +1,4 @@
-from os import system
 import gspread
-import time
-import sys
 
 
 def validate_numeric_input(number_of_options):
@@ -70,7 +67,7 @@ def update_sheet_borders(worksheet, google_sheet):
     """
     Updates the border formatting of a worksheet, supplied as a paramater, in a google sheet. In particular it gives all cells a black border.
     This function will be called when new rows or columns are added to the google sheet.
-    Uses gspread batch_update API method. 
+    Uses gspread batch_update API method.
     """
     borders_update_body = {
         "requests": [
@@ -142,47 +139,8 @@ def update_sheet_borders(worksheet, google_sheet):
                             "alpha": 1
                         }
                     }
-                    
-                } 
+                }
             }
         ]
     }
     google_sheet.batch_update(borders_update_body)
-
-
-def gspread_api_error_exception_handling(gspread_method_or_request):
-    """
-    Decorator function that handles the processing of gspread 'APIError' exceptions that occur as part of a request in a called function or method.
-    If a 'resource exhausted error' occurs, due to exceeding the google sheet API request rate of 60 requests per minute per user,
-    then the request is repeated until successful, which should occur when upto 60 seconds have passed. The user is displayed
-    a timer updating them of the maximum time left for loading. For any other form of API error,
-    an error message is printed, and the program terminated.
-    """
-    def inner(*params, **kparams):
-        time_left = 60
-        while True:
-            try:
-                result = gspread_method_or_request(*params, **kparams)
-                time_left = 60
-            except gspread.exceptions.APIError as error:
-                if error.args[0]['code'] == 429:
-                    system('clear')
-                    print('Loading...')
-                    print(f'Max time left: {time_left}s')
-                    time_left -= 1
-                    time.sleep(1.0)
-                    
-                else:
-                    print('''ERROR ENCOUNTERED: There seems to be a problem accessing the unigrade google sheet. The program will now terminate.
-    Please try running the program again, and try to complete the desired action again. If the error persists try again later.\n''')
-                    print('Enter any key to initiate exiting the unigrade program.')
-                    input('->')
-                    system('clear')
-                    print('Quitting the unigrade program...')
-                    time.sleep(3.0)
-                    system('clear')
-                    sys.exit()
-                    
-            else:
-                return result
-    return inner
